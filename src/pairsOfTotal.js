660496a17a95ea41d33ba2df835fa53ebcc25b8b
mxdavis <md01@me.com>
2017-08-29 23:56:00 +0300

    add udemy super simple solution

--- src/pairsOfTotal.js
+++ src/pairsOfTotal.js
@@ -30,3 +30,21 @@ function numArray2(array, totalNum){
   return sums
 }
 numArray2([1, 6, 4, 5, 3, 3], 7)
+
+function twoSum(numArray, sum) {
+  var pairs = [];
+  var hashTable = [];
+
+  for (var i = 0; i < numArray.length; i++) {
+    var currNum = numArray[i];
+    var counterpart = sum - currNum;
+    if (hashTable.indexOf(counterpart) !== -1) {
+      pairs.push([ currNum, counterpart ]);
+    }
+    hashTable.push(currNum);
+  }
+
+  return pairs;
+}
+
+twoSum([1, 6, 4, 5, 3, 3], 7);
